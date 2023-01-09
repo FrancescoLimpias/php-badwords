42d0d8367c33eb3c0d33da5144b9bd8fb7326e23
Francesco Limpias <francescolimpias@gmail.com>
2023-01-09 12:55:02 +0100

    Base paragraph display

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
+<!-- DATA -->
+<?php
+$paragraph = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni inventore soluta vero at! Ex fugit delectus voluptatum sapiente sequi? At aspernatur laudantium iusto iure mollitia porro corporis aliquam? Incidunt, corrupti?"
+?>
+
+<!-- HTML PAGE -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - Badwords</title>
 </head>
+
 <body>
-    
+    <h1>
+        PHP - Badwords
+    </h1>
+    <p>
+        <?php
+        echo $paragraph;
+        ?>
+    </p>
 </body>
+
 </html>
